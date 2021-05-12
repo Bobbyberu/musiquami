779712b2b95cd0e0cb95c687f40e940d5d02c441
aubin <test>
2021-05-12 16:42:23 +0200

    spotify service documentation :memo:

--- lib/services/SpotifyService.dart
+++ lib/services/SpotifyService.dart
@@ -19,11 +19,8 @@ class SpotifyService {
     this.roomCode = roomCode;
   }
 
-  bool isTokenExpired() {
-    return expiration.isBefore(DateTime.now());
-  }
-
   static Future<SpotifyService> getCredentialsFromCode(String code) async {
+    /// Allow to get tokens from Spotify using code returned during auth
     var secrets = await SecretService.getApiKeys();
 
     final requestBody = {
@@ -53,44 +50,8 @@ class SpotifyService {
     }
   }
 
-  Map<String, String> _buildAuthorizationHeader() {
-    return {'Authorization': 'Bearer $accessToken'};
-  }
-
-  String _buildUrl(String endpoint, Map<String, dynamic> params) {
-    return Uri.https('api.spotify.com', '/v1/$endpoint', params).toString();
-  }
-
-  Future<Response> _requestApi(String url, String method) async {
-    if (expiration.isBefore(DateTime.now())) {
-      await refreshCredentials();
-    }
-    return await Dio().request(url,
-        options: Options(headers: _buildAuthorizationHeader(), method: method));
-  }
-
-  Future<Response> search(String query) async {
-    final params = {'q': query, 'type': 'track', 'limit': '20'};
-    return await _requestApi(_buildUrl('search', params), 'get');
-  }
-
-  Future<Response> queue(String trackUri) async {
-    final params = {'uri': trackUri};
-    return await _requestApi(_buildUrl('me/player/queue', params), 'post');
-  }
-
-  static String getAuthorizeUrl() {
-    final params = {
-      'client_id': _clientId,
-      'response_type': 'code',
-      'redirect_uri': 'http://192.168.0.29:3000/',
-      'scope': 'user-modify-playback-state user-read-private',
-      'show_dialog': 'true'
-    };
-    return Uri.https('accounts.spotify.com', '/authorize', params).toString();
-  }
-
   Future<void> refreshCredentials() async {
+    /// Refresh user's access token and expiration date
     var secrets = await SecretService.getApiKeys();
     final requestBody = {
       'client_id': _clientId,
@@ -109,16 +70,63 @@ class SpotifyService {
     FirebaseService.saveCredentials(this, roomCode);
   }
 
+  void updateCredentials(Map<dynamic, dynamic> credentials) {
+    /// Save new credentials in db
+    accessToken = credentials['accessToken'];
+    expiration = DateTime.fromMillisecondsSinceEpoch(credentials['expiration']);
+  }
+
+  static String getAuthorizeUrl() {
+    /// Build and return url to get access to Spotify connection page
+    final params = {
+      'client_id': _clientId,
+      'response_type': 'code',
+      'redirect_uri': 'http://192.168.0.29:3000/',
+      'scope': 'user-modify-playback-state user-read-private'
+    };
+    return Uri.https('accounts.spotify.com', '/authorize', params).toString();
+  }
+
   // TODO pas static : faire avec token en tant qu'attribut lorsque service instancié
   static Future<Map<String, String>> getUserInfo(String accessToken) async {
+    /// Get user id and product value ('premium' or 'open')
     final response = await Dio().get('https://api.spotify.com/v1/me',
         options: Options(headers: {'Authorization': 'Bearer $accessToken'}));
     return {'id': response.data['id'], 'product': response.data['product']};
   }
 
-  // update service credentials if it has been changed in db
-  void updateCredentials(Map<dynamic, dynamic> credentials) {
-    accessToken = credentials['accessToken'];
-    expiration = DateTime.fromMillisecondsSinceEpoch(credentials['expiration']);
+  Future<Response> search(String query) async {
+    /// Return list of Spotify track given a string search query
+    final params = {'q': query, 'type': 'track', 'limit': '20'};
+    return await _requestApi(_buildUrl('search', params), 'get');
+  }
+
+  Future<Response> queue(String trackUri) async {
+    /// Put track with given uri in user's Spotify queue
+    final params = {'uri': trackUri};
+    return await _requestApi(_buildUrl('me/player/queue', params), 'post');
+  }
+
+  bool isTokenExpired() {
+    /// return true is access token expiration date is before now
+    return expiration.isBefore(DateTime.now());
+  }
+
+  Map<String, String> _buildAuthorizationHeader() {
+    return {'Authorization': 'Bearer $accessToken'};
+  }
+
+  String _buildUrl(String endpoint, Map<String, dynamic> params) {
+    return Uri.https('api.spotify.com', '/v1/$endpoint', params).toString();
+  }
+
+  Future<Response> _requestApi(String url, String method) async {
+    /// perform http call with given url and method (get, post, etc)
+    /// (etc is not an http method, it just means that there are more methods)
+    if (expiration.isBefore(DateTime.now())) {
+      await refreshCredentials();
+    }
+    return await Dio().request(url,
+        options: Options(headers: _buildAuthorizationHeader(), method: method));
   }
 }
